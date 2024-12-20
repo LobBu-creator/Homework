91730460dce02ab9cad4caa36d1a88c6ba7d9736
LobBu-creator <lordpain008@gmail.com>
2024-12-20 17:00:39 +0800

    Update h2extra.js

--- homework2/h2extra.js
+++ homework2/h2extra.js
@@ -1,21 +1,30 @@
-let display = document.getElementById("display");
+$(document).ready(function () {
+    // Show/Hide Instructions
+    $('#toggle-instructions').click(function () {
+        $('#instructions').toggleClass('hidden');
+    });
 
-function appendNumber(number) {
-    display.value += number;
-}
+    // Highlight USD input on focus
+    $('#usd').focus(function () {
+        $(this).css('background-color', '#ffffcc');
+    });
 
-function appendOperator(operator) {
-    display.value += operator;
-}
+    $('#usd').blur(function () {
+        $(this).css('background-color', '');
+    });
 
-function clearDisplay() {
-    display.value = "";
-}
+    // Calculate exchange rates
+    $('#usd').on('input', function () {
+        const usdAmount = parseFloat($(this).val());
 
-function calculateResult() {
-    try {
-        display.value = eval(display.value);
-    } catch (error) {
-        display.value = "Error";
-    }
-}
+        if (!isNaN(usdAmount)) {
+            $('#euro').val((usdAmount * 0.84).toFixed(2));
+            $('#cad').val((usdAmount * 1.23).toFixed(2));
+            $('#hkd').val((usdAmount * 7.76).toFixed(2));
+            $('#jpy').val((usdAmount * 110.80).toFixed(2));
+            $('#mxn').val((usdAmount * 19.82).toFixed(2));
+        } else {
+            $('#euro, #cad, #hkd, #jpy, #mxn').val('');
+        }
+    });
+});
